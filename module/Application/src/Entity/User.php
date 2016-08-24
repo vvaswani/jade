b5c2b1c97d57e3daa0008c62c5b7a49bf0339394
vvaswani <vvaswani@users.noreply.github.com>
2016-08-24 22:22:41 +0530

    Added basic User/Job entities. Closes #6. Closes #7.

---
+++ module/Application/src/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+namespace Application\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    /**
+     * @ORM\Id 
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+ 
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    protected $username;
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $password;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    
+}
