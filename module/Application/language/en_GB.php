72523c68629e4eee3d7f106b515cb82f413248de
vvaswani <vvaswani@users.noreply.github.com>
2016-09-09 03:00:11 +0530

    Updated translation strings

--- module/Application/language/en_GB.php
+++ module/Application/language/en_GB.php
@@ -9,7 +9,11 @@ namespace Application;
 <<<<<<< HEAD
 =======
 		// application-wide labels
+<<<<<<< HEAD
 >>>>>>> Added activity recording service and activity stream in case view
+=======
+<<<<<<< HEAD
+>>>>>>> Updated translation strings
 		'common.create' => 'Create New',
 		'common.view' => 'View',
 		'common.edit' => 'Edit',
@@ -75,6 +79,34 @@ namespace Application;
 		'job.comments' => 'Comments', 
 		'job.entity' => 'Case',
 		'job.current-cases' => 'Current Cases',
+=======
+		'application.common.create' => 'Create New',
+		'application.common.view' => 'View',
+		'application.common.edit' => 'Edit',
+		'application.common.delete' => 'Remove',
+		'application.common.save' => 'Save', 
+		'application.common.cancel' => 'Cancel', 
+		'application.common.dashboard' => 'Dashboard', 
+		'application.common.reports' => 'Reports', 
+		'application.common.settings' => 'Settings', 
+
+		'application.activity-stream.created' => '%s created this case', 
+		'application.activity-stream.deleted' => '%s deleted this case', 
+		'application.activity-stream.updated' => '%s modified the %s from %s to %s', 
+		'application.activity-stream.empty' => 'empty', 
+		
+		// entity specific labels
+		
+		// case entity
+		'application.job.id' => '#', 
+		'application.job.title' => 'Title',
+		'application.job.name' => 'Name', 
+		'application.job.created' => 'Created on',
+		'application.job.description' => 'Description', 
+		'application.job.comments' => 'Comments', 
+		'application.job.entity' => 'Case',
+		'application.job.current-jobs' => 'Current Cases',
+>>>>>>> Updated translation strings
 
 		// label entity
 		'label.id' => '#', 
