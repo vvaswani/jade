f6f0c77b2ade23e1a8591b7e8997ea1c7b753e7d
vvaswani <vvaswani@users.noreply.github.com>
2017-04-01 11:31:00 +0530

    Updated fixture

--- module/Application/src/Fixture/UserFixture.php
+++ module/Application/src/Fixture/UserFixture.php
@@ -13,6 +13,7 @@ class UserFixtureLoader implements FixtureInterface
         $user->setUsername('admin@example.com');
         $user->setName('Administrator');
         $user->setStatus(User::STATUS_ACTIVE);
+        $user->setRole(User::ROLE_ADMINISTRATOR);
         $user->setCreationTime(new \DateTime("now"));
         $user->setPassword('$2y$10$fQ1SOo48Z193Wlnmyq52mec5H4iC/UXmk0DDLMNp.FgmBDplisSZK');
 
