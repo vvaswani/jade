bb613d955649018e84c239b600189d079c2f70e9
vvaswani <vvaswani@users.noreply.github.com>
2019-03-24 02:18:06 +0530

    Updated ignore files

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -10,19 +10,18 @@ if [ ! -f /var/www/html/config/autoload/local.php ]; then
 	echo "Creating configuration file with database credentials..."
 	cp /var/www/html/config/autoload/local.php.dist /var/www/html/config/autoload/local.php
 	sed -i "s/localhost/db/g" /var/www/html/config/autoload/local.php
+	sed -i "s/example-user/$MYSQL_USER/g" /var/www/html/config/autoload/local.php
+	sed -i "s/example-password/$MYSQL_PASSWORD/g" /var/www/html/config/autoload/local.php
+	sed -i "s/example/$MYSQL_DATABASE/g" /var/www/html/config/autoload/local.php
 	echo "Configuration file created."
 else
 	echo "Configuration file found, skipping."
 fi
 
-echo $aa
-
 # wait for database server to become active
-/var/www/html/.docker/scripts/wait-for-it.sh db:3306
-
 # check for database
 # create if not already present
-if [ $? -eq 0 ]; then
+if /var/www/html/.docker/scripts/wait-for-it.sh db:3306 ; then
 	echo "Database server active."
 	echo "Checking for database..."
 	if ! /var/www/html/vendor/bin/doctrine-module dbal:run-sql "SELECT COUNT(*) FROM job" ; then
