01242bfb782f74814756d0f998faa9a833a0fe8b
vvaswani <vvaswani@users.noreply.github.com>
2019-03-23 23:35:38 +0530

    Added Docker entrypoint

---
+++ .docker/scripts/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -e
+
+# check for local configuration file
+# create if not already present
+echo "Checking for database credentials..."
+if [ ! -f /var/www/html/config/autoload/local.php ]; then
+	echo "No database credentials found."
+	echo "Creating configuration file with database credentials..."
+	cp /var/www/html/config/autoload/local.php.dist /var/www/html/config/autoload/local.php
+	sed -i "s/localhost/db/g" /var/www/html/config/autoload/local.php
+	echo "Configuration file created."
+else
+	echo "Configuration file found, skipping."
+fi
+
+echo $aa
+
+# wait for database server to become active
+/var/www/html/.docker/scripts/wait-for-it.sh db:3306
+
+# check for database
+# create if not already present
+if [ $? -eq 0 ]; then
+	echo "Database server active."
+	echo "Checking for database..."
+	if ! /var/www/html/vendor/bin/doctrine-module dbal:run-sql "SELECT COUNT(*) FROM job" ; then
+		echo "No database found."
+		echo "Creating database schema..."
+		/var/www/html/vendor/bin/doctrine-module -n orm:schema-tool:create
+		echo "Loading database fixtures..."
+		/var/www/html/vendor/bin/doctrine-module -n orm:fixtures:load
+	else
+		echo "Database found, skipping database creation."
+	fi
+else
+	echo "Database server inactive, skipping database checks."
+fi
+
+# run default entrypoint commands for parent image
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+        set -- apache2-foreground "$@"
+fi
+
+exec "$@"
+
